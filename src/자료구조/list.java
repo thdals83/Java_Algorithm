edd179b65f37eb68a8bed1d32cce1a732e11ed1b
thdals83 <thdals83@naver.com>
2022-05-08 11:19:09 +0900

    list,set,map

---
+++ src/자료구조/list.java
@@ -0,0 +1,12 @@
+package 자료구조;
+
+import java.util.ArrayList;
+
+public class list {
+    public static void main(String[] args) {
+        //일반 배열
+        
+
+        ArrayList<> list = new ArrayList();
+    }
+}
