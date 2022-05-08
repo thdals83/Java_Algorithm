0d6a43c79faf33610f8b64036d65e5472d649644
thdals83 <thdals83@naver.com>
2022-05-08 11:26:22 +0900

    array, ArrayList

---
+++ src/자료구조/ArrayListtest.java
@@ -0,0 +1,17 @@
+package 자료구조;
+
+import java.util.ArrayList;
+
+public class ArrayListtest {
+    public static void main(String[] args) {
+
+        ArrayList a1 = new ArrayList();
+        a1.add("11");
+        a1.add("22");
+        for (int i = 0; i < a1.size(); i ++){
+            System.out.println(a1.get(i));
+        }
+    }
+    //pvsm
+
+}
---
+++ src/자료구조/array.java
@@ -0,0 +1,15 @@
+package 자료구조;
+
+public class array {
+    public static void main(String[] args) {
+        //일반 배열
+        String[] arrayobj = new String[2];
+        arrayobj[0] = "11";
+        arrayobj[1] = "22";
+        for (int i = 0; i< arrayobj.length; i ++){
+            System.out.println(arrayobj[i]);
+        }
+
+
+    }
+}
